80b9bcc40d73d3f92840f74a59582c9feca36386
Hamza Sohail SCM <hsoahil.mscs21seecs@seecs.edu.pk>
2022-06-11 01:59:42 +0500

    progess bar for showing data ussage

--- src/components/imagesViewer/ImageUploader.jsx
+++ src/components/imagesViewer/ImageUploader.jsx
@@ -3,139 +3,166 @@ import { BlobServiceClient } from '@azure/storage-blob';
 import { createImage, getAllImages } from '../../commonServices';
 import { useNavigate } from 'react-router-dom';
 
+let imagesize = 0;
 export default function ImageUploader() {
-  const navigate = useNavigate();
-  const containerName = `tutorial-container`;
-  const sasToken = process.env.REACT_APP_STORAGESASTOKEN;
-  const storageAccountName = process.env.REACT_APP_STORAGERESOURCENAME;
-  const [fileSelected, setFileSelected] = useState(null);
-  const [uploading, setUploading] = useState(false);
-  const [blobList, setBlobList] = useState([]);
-  const [inputKey, setInputKey] = useState('');
-  const [imagesList, setImagesList] = useState([]);
+	const navigate = useNavigate();
+	const containerName = `tutorial-container`;
+	const sasToken = process.env.REACT_APP_STORAGESASTOKEN;
+	const storageAccountName = process.env.REACT_APP_STORAGERESOURCENAME;
+	const [fileSelected, setFileSelected] = useState(null);
+	const [uploading, setUploading] = useState(false);
+	const [blobList, setBlobList] = useState([]);
+	const [inputKey, setInputKey] = useState('');
+	const [imagesList, setImagesList] = useState([]);
+	async function getImages() {
+		const user = JSON.parse(localStorage.getItem('user'));
+		const getImages = await getAllImages(user._id);
+		setImagesList(getImages.data);
+	}
+	useEffect(async () => {
+		await getImages();
+	}, []);
 
-  useEffect(async () => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    const getImages = await getAllImages(user._id);
-    setImagesList(getImages.data);
-  }, []);
+	const createBlobInContainer = async (containerClient, file) => {
+		// create blobClient for container
+		const blobClient = containerClient.getBlockBlobClient(file.name);
 
-  const createBlobInContainer = async (containerClient, file) => {
-    // create blobClient for container
-    const blobClient = containerClient.getBlockBlobClient(file.name);
+		// set mimetype as determined from browser with file upload control
+		const options = { blobHTTPHeaders: { blobContentType: file.type } };
 
-    // set mimetype as determined from browser with file upload control
-    const options = { blobHTTPHeaders: { blobContentType: file.type } };
+		// upload file
+		await blobClient.uploadData(file, options);
+	};
 
-    // upload file
-    await blobClient.uploadData(file, options);
-  };
+	const getBlobsInContainer = async (containerClient) => {
+		const returnedBlobUrls = [];
 
-  const getBlobsInContainer = async (containerClient) => {
-    const returnedBlobUrls = [];
+		// get list of blobs in container
+		// eslint-disable-next-line
+		for await (const blob of containerClient.listBlobsFlat()) {
+			// if image is public, just construct URL
+			returnedBlobUrls.push(
+				`https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
+			);
+		}
 
-    // get list of blobs in container
-    // eslint-disable-next-line
-    for await (const blob of containerClient.listBlobsFlat()) {
-      // if image is public, just construct URL
-      returnedBlobUrls.push(
-        `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
-      );
-    }
+		return returnedBlobUrls;
+	};
+	// <snippet_uploadFileToBlob>
+	const uploadFileToBlob = async (file) => {
+		if (!file) return [];
 
-    return returnedBlobUrls;
-  };
-  // <snippet_uploadFileToBlob>
-  const uploadFileToBlob = async (file) => {
-    if (!file) return [];
+		// get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
+		const blobService = new BlobServiceClient(
+			`https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
+		);
 
-    // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
-    const blobService = new BlobServiceClient(
-      `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
-    );
+		// get Container - full public read access
+		const containerClient = blobService.getContainerClient(containerName);
+		await containerClient.createIfNotExists({
+			access: 'container',
+		});
 
-    // get Container - full public read access
-    const containerClient = blobService.getContainerClient(containerName);
-    await containerClient.createIfNotExists({
-      access: 'container',
-    });
+		// upload file
+		await createBlobInContainer(containerClient, file);
 
-    // upload file
-    await createBlobInContainer(containerClient, file);
+		// get list of blobs in container
+		return getBlobsInContainer(containerClient);
+	};
 
-    // get list of blobs in container
-    return getBlobsInContainer(containerClient);
-  };
+	const onFileChange = async (event) => {
+		// capture file into state
+		console.log(imagesize + event.target.files[0].size / 1000 / 1000);
+		if (imagesize + event.target.files[0].size / 1000 / 1000 < 8) {
+			alert(
+				'You have used more than 80% of your space please delete some images.'
+			);
+			return;
+		} else if (imagesize + event.target.files[0].size / 1000 / 1000 > 10) {
+			alert('You have used 100% space please delete some images.');
+			return;
+		}
 
-  const onFileChange = async (event) => {
-    // capture file into state
-    setFileSelected(event.target.files[0]);
-    console.log({ eventFile: event.target.files });
-    const user = JSON.parse(localStorage.getItem('user'));
-    const imageObj = {
-      fileName: event.target.files[0].name,
-      fileSize: event.target.files[0].size / 1000 / 1000,
-      createdBy: user._id,
-    };
-    const res = await createImage(imageObj);
-    console.log({ res });
-  };
+		setFileSelected(event.target.files[0]);
+		console.log({ eventFile: event.target.files });
+		const user = JSON.parse(localStorage.getItem('user'));
+		const imageObj = {
+			fileName: event.target.files[0].name,
+			fileSize: event.target.files[0].size / 1000 / 1000,
+			createdBy: user._id,
+		};
+		const res = await createImage(imageObj);
+		console.log({ res });
+	};
 
-  const onFileUpload = async () => {
-    // prepare UI
-    setUploading(true);
+	const onFileUpload = async () => {
+		// prepare UI
+		setUploading(true);
 
-    // *** UPLOAD TO AZURE STORAGE ***
-    const blobsInContainer = await uploadFileToBlob(fileSelected);
+		// *** UPLOAD TO AZURE STORAGE ***
+		const blobsInContainer = await uploadFileToBlob(fileSelected);
 
-    // prepare UI for results
-    setBlobList(blobsInContainer);
+		// prepare UI for results
+		setBlobList(blobsInContainer);
 
-    // reset state/form
-    setFileSelected(null);
-    setUploading(false);
-    setInputKey(Math.random().toString(36));
-  };
+		// reset state/form
+		setFileSelected(null);
+		setUploading(false);
+		await getImages();
+		setInputKey(Math.random().toString(36));
+	};
 
-  const onLogout = async () => {
-    // prepare UI
-    navigate('/');
-  };
-  return (
-    <div>
-      <div style={{ margin: '20px' }}>
-        <h4>Choose an Image to upload</h4>
+	const onLogout = async () => {
+		// prepare UI
+		navigate('/');
+	};
+	imagesize = imagesList.length
+		? imagesList.reduce((acc, item) => acc + item.fileSize, 0).toFixed(2) || 0
+		: 0;
+	const allImageSize = (imagesize / 10) * 100 || 0;
+	return (
+		<div style={{ margin: '20px' }}>
+			<div style={{ margin: '20px' }}>
+				<h4>Choose an Image to upload</h4>
 
-        <input type="file" onChange={onFileChange} key={inputKey} />
-        <button type="submit" onClick={onFileUpload}>
-          Upload!
-        </button>
-        <button
-          style={{
-            width: '100px',
-            height: '30px',
-            marginLeft: '75%',
-            backgroundColor: 'blue',
-            color: 'white',
-          }}
-          onClick={onLogout}
-        >
-          logout
-        </button>
+				<input type="file" onChange={onFileChange} key={inputKey} />
+				<button type="submit" onClick={onFileUpload}>
+					Upload!
+				</button>
+				<button
+					style={{
+						width: '100px',
+						height: '30px',
+						marginLeft: '75%',
+						backgroundColor: 'blue',
+						color: 'white',
+					}}
+					onClick={onLogout}
+				>
+					logout
+				</button>
 
-        {uploading && '    . . . File Uploading. Please Wait'}
-      </div>
-      <hr></hr>
-      <h4>Uploaded Images</h4>
-      {imagesList.map((item, index) => (
-        <img
-          src={`https://cloudimagestorage.blob.core.windows.net/tutorial-container/${item.fileName}`}
-          width={200}
-          height={200}
-          key={item.fileName + index}
-          style={{ margin: '20px' }}
-        />
-      ))}
-    </div>
-  );
+				{uploading && '    . . . File Uploading. Please Wait'}
+			</div>
+			<div style={{ margin: '20px' }}>
+				<hr></hr>
+				<h3>Storage</h3>
+				You have used cloud storage of {imagesize} MB out of 10MB.
+				<br />
+				<label for="file">Used Storage:</label>
+				<progress id="file" value={allImageSize} max="100"></progress>
+				{allImageSize.toFixed(2)}%<hr></hr>
+			</div>
+			<h4>Uploaded Images</h4>
+			{imagesList.map((item, index) => (
+				<img
+					src={`https://cloudimagestorage.blob.core.windows.net/tutorial-container/${item.fileName}`}
+					width={200}
+					height={200}
+					key={item.fileName + index}
+					style={{ margin: '20px' }}
+				/>
+			))}
+		</div>
+	);
 }
--- src/components/register/RegisterPage.jsx
+++ src/components/register/RegisterPage.jsx
@@ -4,75 +4,75 @@ import { Signup } from '../../commonServices';
 import '../../App.css';
 
 export default function SignUpPage() {
-  const [response, setResponse] = useState(null);
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState(false);
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const navigate = useNavigate();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setError(false);
-    const temp = await Signup({ email, password, name: username });
-    setResponse(temp);
-    if (temp.status === 201) {
-      navigate('/login');
-    } else {
-      setError(true);
-    }
-  };
-  return (
-    <div className="text-center m-5-auto">
-      <h2>Join us</h2>
-      <h5>Create your personal account</h5>
-      <form>
-        <p>
-          <label>Username</label>
-          <br />
-          <input
-            type="text"
-            name="first_name"
-            required
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </p>
-        <p>
-          <label>Email address</label>
-          <br />
-          <input
-            type="email"
-            name="email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </p>
-        <p>
-          <button id="sub_btn" type="submit" onClick={handleSubmit}>
-            Register
-          </button>
-        </p>
-      </form>
-      <footer>
-        <p>
-          {error && (
-            <p style={{ color: 'red' }}>
-              Invalid Data Entered or email already exist
-            </p>
-          )}
-          <Link to="/">Back to Homepage</Link>.
-        </p>
-      </footer>
-      {console.log({ response })}
-    </div>
-  );
+	const [response, setResponse] = useState(null);
+	const [email, setEmail] = useState('');
+	const [error, setError] = useState(false);
+	const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const navigate = useNavigate();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		setError(false);
+		const temp = await Signup({ email, password, name: username });
+		setResponse(temp);
+		if (temp.status === 201) {
+			navigate('/');
+		} else {
+			setError(true);
+		}
+	};
+	return (
+		<div className="text-center m-5-auto">
+			<h2>Join us</h2>
+			<h5>Create your personal account</h5>
+			<form>
+				<p>
+					<label>Username</label>
+					<br />
+					<input
+						type="text"
+						name="first_name"
+						required
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</p>
+				<p>
+					<label>Email address</label>
+					<br />
+					<input
+						type="email"
+						name="email"
+						required
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</p>
+				<p>
+					<label>Password</label>
+					<br />
+					<input
+						type="password"
+						name="password"
+						required
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</p>
+				<p>
+					<button id="sub_btn" type="submit" onClick={handleSubmit}>
+						Register
+					</button>
+				</p>
+			</form>
+			<footer>
+				<p>
+					{error && (
+						<p style={{ color: 'red' }}>
+							Invalid Data Entered or email already exist
+						</p>
+					)}
+					<Link to="/">Back to Homepage</Link>.
+				</p>
+			</footer>
+			{console.log({ response })}
+		</div>
+	);
 }
